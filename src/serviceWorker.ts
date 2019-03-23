1f595f27d536188a95efa0d1ae0bdcf85760b165
Fer <fergalmonte@hotmail.com>
2019-03-22 21:35:15 -0600

    Prueba

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -12,6 +12,8 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
+
+  window.location.hostname === process.env.PORT ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
